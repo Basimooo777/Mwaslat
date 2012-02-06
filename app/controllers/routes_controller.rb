0aa6013f5ef25385f688c7d8f74894b916c43f04
Ahmed Basseem <Ahmed.Basseem@gmail.com>
2012-02-06 10:09:49 +0200

    First_Commit_Basimoooo777

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,16 +1,20 @@
 class RoutesController < ApplicationController
+
   def new
+
     @route = Route.new
     @routes = Route.all
+
   end
+  
   def create
+    
     @route = Route.new(params[:route])
     @trans = Transportation.new(params[:trans])
     @src = Node.new(params[:src])
     @dest = Node.new(params[:dest])
 
     # For inserting the streets
-        
     count_src = Integer(params[:count_src])
     count_dest = Integer(params[:count_dest])
     for i in 1..count_src do
@@ -19,23 +23,27 @@ class RoutesController < ApplicationController
       @street.node = @src
       @street.save
     end
+
     for i in 1..count_dest do
       str = "street_dest#{i}"
       @street = Street.new(:name => params[str])
       @street.node = @dest
       @street.save
     end
+
   # For the points and nodes
     src_lng = params[:src_lng].split(",");
     src_lat = params[:src_lat].split(",");
     dest_lng = params[:dest_lng].split(",");
     dest_lat = params[:dest_lat].split(",");
+
     # For source points  
     for i in 0...src_lng.length do
       @point = Point.new(:longitude => Float(src_lng[i]), :latitude => Float(src_lat[i]))
       @point.node = @src
       @point.save
     end
+
     # For destination points  
     for i in 0...dest_lng.length do
       @point = Point.new(:longitude => Float(dest_lng[i]), :latitude => Float(dest_lat[i]))
@@ -56,10 +64,9 @@ class RoutesController < ApplicationController
   
   def search
     if(params[:src] != nil)
-        # @src = Node.where(:district => params[:src])
-        # @dest = Node.where(:district => params[:dest])
         @routes = Route.find_by_sql("select * from routes where src_id in 
         (select id from nodes where district='#{params[:src]}') and dest_id in (select id from nodes where district='#{params[:dest]}')")
     end
   end
+
 end
