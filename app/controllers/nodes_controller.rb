214ab5ea593b261fff00079b7a845b2dab59a347
M.Shehata <shehata.mohammed@gmail.com>
2012-05-01 18:02:43 +0200

    modify authentication for enable auto complete

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,5 +1,5 @@
 class NodesController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:districts]
 
   def index
     @nodes = Node.where(:user_id => '1')
